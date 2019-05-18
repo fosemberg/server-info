5df20987e0fca619dd6989f4cbeb62ef80132db0
fosemberg <fosemberg@gmaill.com>
2019-05-18 23:00:54 +0300

    download json from firebase with jsonp

--- src/components/LoadButton/ExportButton.tsx
+++ src/components/LoadButton/ExportButton.tsx
@@ -3,19 +3,46 @@ import {Button} from "react-bootstrap";
 import {stateUrl} from "../../store";
 import './ExportButton.css';
 
+class ExportButton extends React.PureComponent<{}, {}> {
+  private jsonpCallbackName = 'getExportJson'
 
-const ExportButton = () => {
-  return (
-    <div className='export-button'>
-      <Button
-        variant="outline-secondary"
-        href={stateUrl}
-        type='button'
-      >
-        Экспортировать данные
-      </Button>
-    </div>
-  );
-};
+  public componentDidMount() {
+    (window as any)[this.jsonpCallbackName] = (exportObj: any) => {
+      const exportName = 'export';
+      const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+      const downloadAnchorNode = document.createElement('a');
+      downloadAnchorNode.setAttribute("href", dataStr);
+      downloadAnchorNode.setAttribute("download", exportName + ".json");
+      document.body.appendChild(downloadAnchorNode); // required for firefox
+      downloadAnchorNode.click();
+      downloadAnchorNode.remove();
+    }
+  }
+
+  public render() {
+    return (
+      <div className='export-button'>
+        <a
+          onClick={this.download}
+        >
+          <Button
+            variant="outline-secondary"
+            type='button'
+          >
+            Экспортировать данные
+          </Button>
+        </a>
+      </div>
+    );
+  }
+
+  private download = () => {
+    // jsonp download
+    const script = document.createElement("script");
+    script.src = `${stateUrl}/.json?callback=${this.jsonpCallbackName}`;
+    script.async = true;
+    document.body.appendChild(script);
+  }
+}
 
 export default ExportButton;
